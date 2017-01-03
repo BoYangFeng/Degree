12fcda27f17f002bb45aa423af65c8fbfd724435
BYF <moon_boyangfeng@sina.com>
2017-01-03 17:39:05 +0800

    no message

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,17 +1,19 @@
+# -*- coding:utf8 -*-
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
+class NameForm(Form):
+    name = StringField(u'你叫什么名字?',validators=[Required()])
+    submit = SubmitField(u'提交')
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),Email()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
-
-
 
 class RegistrationForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64),Email()])
@@ -21,7 +23,7 @@ class RegistrationForm(Form):
                                       'numbers, dots or underscores')])
     password = PasswordField('Password', validators=[Required(),
                                                      EqualTo('password2',
-                                                             message='Passwords must match.')])
+                                                     message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[Required()])
     submit = SubmitField('Register')
 
